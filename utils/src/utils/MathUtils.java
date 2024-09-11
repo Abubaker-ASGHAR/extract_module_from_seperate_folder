c2f42d53ce48a0746ae3980e1be9e2af8079b786
abubaker <abubaker.asghar@techno-soft.com>
2024-09-11 16:07:44 +0500

    second commit

---
+++ utils/src/utils/MathUtils.java
@@ -0,0 +1,7 @@
+package utils;// No package declaration if src is directly under module root
+
+public class MathUtils {
+    public static int square(int num) {
+        return num * num;
+    }
+}
